04a593a6c7db2d07036f15e0658cfe06f64c3035
el mehhdi <lmehdi0006@gmail.com>
2024-09-28 18:46:46 +0100

    pool prep

---
+++ 1_printalphabet.py
@@ -0,0 +1,24 @@
+# Initialize wa7ed string khawi njem3o fih l'alphabet
+alphabet = ''
+
+# Iterate through the ASCII values men 'a' l 'z' (men 97 l 122) 
+for i in range(97, 123):
+    # n'convertéw ascii l charactere a l'aide de chr()
+    alphabet += chr(i)
+
+# Print the resulting string
+print(alphabet)
+
+
+
+
+'''
+# men 65 l 90 bach tprinté l majuscule
+alphabet = ''
+
+for i in range (65,91):
+    alphabet += chr(i)
+
+print(alphabet)
+
+'''
